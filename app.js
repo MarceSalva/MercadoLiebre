5b9b251874dd91b02a282e3a6791cce3e2b54b00
Marcela Salvatierra <mksalv05@gmail.com>
2023-05-21 15:05:06 -0300

    Subida del domingo 21/05

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ app.listen(3000, () => {
 app.get('/' , (rep, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
+
+app.get('/register' , (rep, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get('/login' , (rep, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
